2cc07b342145e12ea1898facf8ca3027ee7cf8cc
AJIT TORANE <145534227+ajittorane@users.noreply.github.com>
2025-05-28 11:07:54 +0530

    Delete Grocery Store directory

--- Grocery Store/admin_logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['admin_id']); // Remove only the admin session
-header("Location: admin_login.php"); // Redirect to admin login page
-exit();
-?>
--- Grocery Store/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['user_id']); // Remove only user session
-header("Location: home.php"); // Redirect user to homepage
-exit();
-?>
