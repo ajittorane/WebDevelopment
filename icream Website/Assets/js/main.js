4eac7b06eca2c544df1b0ece97b76725e66162a6
AJIT TORANE <145534227+ajittorane@users.noreply.github.com>
2025-11-03 12:13:07 +0530

    Add files via upload

---
+++ icream Website/Assets/js/main.js
@@ -0,0 +1,192 @@
+(function ($) {
+    "use strict";
+    
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Service and team carousel
+    $(".service-carousel, .team-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+    // Product carousel
+    $(".product-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            }
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+    
+})(jQuery);
+
+
+//SEND MESSAGE
+
+let url = 'https://script.google.com/macros/s/AKfycbwYcZ9VrUELHuuFmI3yobpZn0-8PrAlRB4SHnO12EHftQRfpdLVCIrc7TrKo-Hh2Gr5zQ/exec';
+let form = document.querySelector('#form');
+let loading = document.querySelector('.loading');
+let resBox = document.getElementById('res');
+let btn = form.querySelector('button[type="submit"]');
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Show loading animation and change button text
+  loading.style.display = "block";
+  btn.disabled = true;
+  btn.innerHTML = "Submitting...";
+
+  let d = new FormData(form);
+
+  fetch(url, {
+    method: "POST",
+    body: d
+  })
+  .then((res) => res.text())
+  .then((finalRes) => {
+    // Show response message
+    resBox.innerHTML = finalRes;
+    form.reset();
+  })
+  .catch((err) => {
+    resBox.innerHTML = "Submission failed. Please try again.";
+    console.error("Error:", err);
+  })
+  .finally(() => {
+    // Hide loading and reset UI
+    loading.style.display = "none";
+    btn.disabled = false;
+    btn.innerHTML = "Send Message";
+
+    // Clear message after 5 seconds
+    setTimeout(() => {
+      resBox.innerHTML = '';
+    }, 5000);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
